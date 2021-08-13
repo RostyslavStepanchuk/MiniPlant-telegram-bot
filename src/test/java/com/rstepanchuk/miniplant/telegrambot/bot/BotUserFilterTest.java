1350ed75f6851c27899acac3f548bae7fe9f8176
Rostyslav Stepanchuk <rostyslav.stepanchuk@gmail.com>
2021-08-13 22:11:11 +0300

    coveralls integration

--- src/test/java/com/rstepanchuk/miniplant/telegrambot/bot/BotUserFilterTest.java
+++ src/test/java/com/rstepanchuk/miniplant/telegrambot/bot/BotUserFilterTest.java
@@ -1,30 +1,15 @@
 package com.rstepanchuk.miniplant.telegrambot.bot;
 
-import com.rstepanchuk.miniplant.telegrambot.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
 class BotUserFilterTest {
 
-  @InjectMocks
   private BotUserFilter botUserFilter;
-  @Mock
-  private UserRepository userRepository;
 
   @Test
-  void nullParamShouldThrowNullPointer() {
+  void name() {
     assertThrows(NullPointerException.class, ()-> botUserFilter.processUpdate(null));
-  }
-
-  @Test
-  void dummyTest() {
-    boolean test = true;
-    assertTrue(test);
   }
 }
