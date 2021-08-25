fd816c5172930cb93476f5910e794a5182a61d55
Rostyslav Stepanchuk <rostyslav.stepanchuk@gmail.com>
2021-08-25 09:54:45 +0300

    Minor fixes for SonarCloud

--- src/main/java/com/rstepanchuk/miniplant/telegrambot/bot/stages/DialogStage.java
+++ src/main/java/com/rstepanchuk/miniplant/telegrambot/bot/stages/DialogStage.java
@@ -1,8 +1,5 @@
 package com.rstepanchuk.miniplant.telegrambot.bot.stages;
 
-import com.rstepanchuk.miniplant.telegrambot.util.Constants;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
 public interface DialogStage {
 
 //  void executeStage(Update update);
